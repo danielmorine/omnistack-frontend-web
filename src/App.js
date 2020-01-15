29bb41cf2fe1c7b6597095f68922fa55abb84942
Daniel Haro <contato.danielharo@gmail.com>
2020-01-15 09:00:26 -0300

    FrontEnd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import './Global.css';
+
 import './App.css';
+import './Sidebar.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+              <label htmlFor="Nome">Usuário GitHub</label>
+              <input name="gitHubUser" id="gitHubUser"/>
+          </div>
+          <div className="input-block">
+              <label htmlFor="technology">Tecnologias</label>
+              <input name="technology" id="technology"/>
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+                <label htmlFor="Nome">Latitude</label>
+                <input name="gitHubUser" id="gitHubUser"/>
+            </div>
+            <div className="input-block">
+                <label htmlFor="Nome">Longitude</label>
+                <input name="gitHubUser" id="gitHubUser"/>
+            </div>
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
+      </aside>
+      <main></main>
     </div>
   );
 }
